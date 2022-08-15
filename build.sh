62736452769e35bbfd87afce9156a0fa71958b88
Jonathan BEN-AVRAHAM <jonathan.ba@siklu.com>
2022-08-15 17:42:37 +0300

    PORTF-1127 Added CACHE_ROOT_DIR envar
    Default CACHE_ROOT_DIR=${HOME}

--- build.sh
+++ build.sh
@@ -5,10 +5,26 @@
 # Set PROJECT_ROOT_DIR to some place where you have checked out the portfolio
 # repo so that you will have access to the scripts that are used in Siklu's
 # additions to Buildroot. This variable does not affect the place where
-# Buildroot isbuilt, which is in the current directory where this script
+# Buildroot is built, which is in the current directory where this script
 # resides. [The requirement is to define this variable in the Buildroot
 # directory is a Siklu design flaw that should be fixed.]
 export PROJECT_ROOT_DIR="$(pwd)"
+
+# Before running this script you must set CACHE_ROOT_DIR to a directory in a
+# partition that has sufficient space for the ccache and Buildroot dl directory
+# for your rootfs configuration.
+if [ -z "${CACHE_ROOT_DIR+x}" ]
+then
+	echo "ERROR: Before running this script please export the variable"
+	echo "CACHE_ROOT_DIR who's value should be a dirname that will hold"
+	echo "the ccache and Buildroot dl directories."
+	exit 1
+fi
+if [ ! -d "${CACHE_ROOT_DIR}" ]
+then
+	echo "ERROR: The envar CACHE_ROOT_DIR is not set to a dirname."
+	exit 1
+fi
 
 # This path for the cross compiler is the Siklu standard
 export CROSS_COMPILE=/opt/arm/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
@@ -16,8 +32,8 @@ export PATH=/opt/arm/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf/bin/:$
 
 # Temporarily comment out whatever you don't need.
 #make oldconfig
+make siklu_80X0_defconfig
 #make menuconfig
 #make savedefconfig
-make siklu_80X0_defconfig
 make clean
 make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE}
